6ccf08a65dbbf0d210bf693e207111db269f89a1
Jeremy Yang <jeremyjyang@gmail.com>
2022-12-15 14:45:47 -0700

    Alright.

--- sh/Go_gsrs_Test.sh
+++ sh/Go_gsrs_Test.sh
@@ -24,6 +24,29 @@ printf "N_smiles = ${N}\n"
 #
 psql -d $DBNAME -c "SET rdkit.morgan_fp_size=2048"
 #
+###
+# Workaround due to RDKit bug:
+# 41302. UNII: 9S1BC7865F; SMILES: "[Cl-].[Cl-].[Cl-].[Cl-].[Cl-].[Nd+5]"
+# UPDATE unii_records SET cansmi = mol_to_smiles(molecule) WHERE unii = '9S1BC7865F'
+# ERROR:  molFromPickle: invalid value in pickle
+# 
+# 83049. UNII: T86H76439H; SMILES: "[F-].[F-].[F-].[F-].[F-].[Nd+5]"
+# UPDATE unii_records SET cansmi = mol_to_smiles(molecule) WHERE unii = 'T86H76439H'
+# ERROR:  molFromPickle: invalid value in pickle
+###
+# gsrs=> select mol_from_smiles('[Cl-].[Cl-].[Cl-].[Cl-].[Cl-].[Nd+5]') ;
+# ERROR:  molFromPickle: invalid value in pickle
+# gsrs=> select mol_from_smiles('[F-].[F-].[F-].[F-].[F-].[Nd+5]') ;
+# ERROR:  molFromPickle: invalid value in pickle
+###
+problem_uniis="\
+9S1BC7865F \
+T86H76439H \
+"
+for unii in $problem_uniis ; do
+	psql -e -d $DBNAME -c "UPDATE unii_records SET molecule = NULL WHERE unii = '${unii}'"
+done
+#
 I=0
 while [ $I -lt $N ]; do
 	I=$[$I + 1]
@@ -35,6 +58,7 @@ while [ $I -lt $N ]; do
 	psql -e -d $DBNAME -c "UPDATE unii_records SET fp = morganbv_fp(molecule, 2) WHERE unii = '${unii}'"
 done
 #
+
 #
 ###
 printf "Elapsed: %ds\n" "$[$(date +%s) - $T0]"
